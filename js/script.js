4567632e0efedfbfe33ee594bec2fdf89cbdc0ff
JiHwayeon <90582529+JiHwayeon@users.noreply.github.com>
2021-10-07 12:32:30 +0900

    js수정

--- js/script.js
+++ js/script.js
@@ -10,6 +10,47 @@
       $(".leftWrap").animate({"left" : "-100%"}, 500);
     });
    
+    var swiper = new Swiper(".mySwiper01", {
+      direction: "vertical",
+      slidesPerView: 1,
+      // spaceBetween: 30,
+      mousewheel: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        nested: true
+      },
+      //콜백함수 - 슬라이드가 바뀔때마다 체크
+      on: {
+        slideChangeTransitionStart: function () {
+        if($(".swiper-slide.con").hasClass("swiper-slide-active") === true){
+          $(".swiper-slide.con").find(".sec").addClass("on")
+        }else{
+          $(".swiper-slide.con").find(".sec").removeClass("on")
+        } if($(".swiper-slide.recruit").hasClass("swiper-slide-active") === true){
+          $(".swiper-slide.recruit").find(".sec02").addClass("on")
+        }else{
+          $(".swiper-slide.recruit").find(".sec02").removeClass("on")
+        } if($(".swiper-slide.last").hasClass("swiper-slide-active") === true){
+          $(".swiper-slide.last").find(".sec03").addClass("on")
+        }else{
+          $(".swiper-slide.last").find(".sec03").removeClass("on")
+        }
+
+      }}
+    });
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      slidesPerGroup: 2,
+      loop: true,
+      loopFillGroupWithBlank: true,
+   
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
 
     
       
